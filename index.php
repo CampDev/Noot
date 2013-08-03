fa76d455c715d4a8c6d6b3900bd407eacf187143
Kevin <kevin_kleinman@live.nl>
2013-08-03 16:39:09 +0200

    Widths based on percentages; scales nicely

--- index.php
+++ index.php
@@ -27,7 +27,8 @@ require_once('tent-markdown.php');
 	<?php include('header.php'); ?>
 
 			<div class="container">
-				<div class="sidebar">
+				<div class="column-1">
+                <div class='column-1-inner'>
                     <div style='padding: 5%; margin-top: 5px; border-bottom: 1px solid #ddd;'><a href="index.php"><b>All notebooks</b></a></div>
 
 
@@ -51,7 +52,7 @@ require_once('tent-markdown.php');
 						$content = $notebook['content'];
 						echo "";
 						if (!is_null($content['name'])) {
-							echo "<div style='width: 90%; padding: 5%; border-bottom: 1px solid #ddd;'><a href='index.php?notebook=".$notebook['id']."'>".$content['name']."</a>";
+							echo "<div style='width: 90%; padding: 5%; border-bottom: 1px solid #ddd;'><img src='img/notebook.png' style='width: 16px; margin-right: 5px; margin-bottom: -3px;'><a href='index.php?notebook=".$notebook['id']."'>".$content['name']."</a>";
 							echo "<a style='float: right;' href='edit.php?notebook=".$notebook['id']."'>Edit</a>";
 							echo "<a class='delete' href='task_handler.php?type=delete&id=".$notebook['id']."'><img src='img/delete.png' style='width: 8px; float: right; margin: 3px;'></a></div>"; 
 						}
@@ -61,15 +62,14 @@ require_once('tent-markdown.php');
 					echo "<form align='center' method='post' action='task_handler.php?type=notebook' style='margin-top: 30px;'>
 						<input type='text' name='notebook_name' placeholder='Add new notebook' class='text' style='width: 70%'/>
 						<input type='submit' class='text' style='width: 28%;'>
-					</form></div>";
+					</form></div></div>";
 					}
 				?>
 
 
 
-                		</div>
-
-				<div class='note-list' style="height: 100%;">
+				<div class='column-2' style="height: 100%;">
+                <div class='column-2-inner'>
 
 				<div class="filters">
                 <div style="float: left;">"Notebook title" / "All notes"</div>
@@ -97,7 +97,7 @@ require_once('tent-markdown.php');
 
 						echo "</tr>";
 					}
-					echo "</table></div>";
+					echo "</table></div></div>";
 					if (isset($_GET['note'])) {
 						$id = $_GET['note'];
 						$nonce = uniqid('Noot_', true);
@@ -116,14 +116,17 @@ require_once('tent-markdown.php');
 						$current_note = json_decode($current_note_json, true);
 						?>
 
-				<div class='note-list' style="height: 100%; max-width: 700px;">
-				<div class="filters" style="max-width: 674px;">
+				<div class='column-3'>
+                <div class='column-3-inner'>
+				<div class="filters" style="max-width: 721px;">
                 <div style="float: left;"><?php echo $current_note['post']['content']['title']; ?></div><a href="index.php?note=<?php echo $current_note['post']['id']; ?>&edit=true">Edit</a> | <a href="task_handler.php?type=delete&id=<?php echo $current_note['post']['id']; ?>">Delete</a>
                 </div>
-                <div style="padding: 20px;">
+                <div style="padding: 3%;">
                         <?php echo tent_markdown($current_note['post']['content']['body']); ?>
                 </div>
                 </div>
+                </div>
+                
 
 <!--						<form align="center" method="task_handler.php?type=update&id=<?php echo $current_note['post']['id']; ?>" method="post">
 							<p><input class="note_title" type="text" name="title" value="<?php echo $current_note['post']['content']['title']; ?>" /></p>
@@ -178,6 +181,7 @@ require_once('tent-markdown.php');
 				}
 				?>
         </div>
+                		</div>
 		<?php include_once('footer.php') ?>
 
 	</body>
