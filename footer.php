2e47481c272e84b204ab198749c17f0b5bd57b1a
Cacauu <achternbosch@me.com>
2013-08-04 11:05:28 +0200

    Removed list.php which wasn't used

--- footer.php
+++ footer.php
@@ -3,5 +3,4 @@
         Developed by <a href="http://dev.campnews.org">^CampDev</a> - <a href="developer.php">Developer Resources</a> - <a href="logout.php">Logout</a>
         </div>
         <div id="new_post" style="display: none;"><?php include('new_post_page.php') ?></div>
-        <div id="list_management" style="display: none;"><?php include('list.php') ?></div>
 		</footer>
